9814c0ed025ae13d2396184c735ec9a80d50bdf5
Vasile <popovvasile@gmail.com>
2023-11-21 09:00:38 +0200

    small changes

--- data_parser/mapping.py
+++ data_parser/mapping.py
@@ -70,7 +70,7 @@ for ind, event in enumerate(content[1:]):
     })
     print([event[-8]])
     list_of_sources[f"sources{ind}"] = {"id": f"sources{ind}", "paths": [event[-9]], "title": "", "description":""}
-    list_of_associations.append({"id": f"associations{ind}", "desc": [event[-8]], "title": [event[-8]], "mode": "FILTER", "desc": "", "filter_paths": [event[-8]]})
+    list_of_associations.append({"id": f"associations{ind}", "desc": [event[-8]], "title": [event[-8]], "mode": "FILTER", "filter_paths": [event[-8]]})
 list_of_events = {"Events": list_of_events, "list_of_sources": list_of_sources, "Associations":list_of_associations}
 # print(list_of_wrong)
 with open("list_of_events.json", "w") as f:
