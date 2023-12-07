01a1bf5e337219cbfb190d99334f2c15e537a78b
Your Name <you@example.com>
2023-12-07 17:16:46 -0500

    authorization to json server added

--- data_parser/server.mjs
+++ data_parser/server.mjs
@@ -1,4 +1,5 @@
 import pkg from 'json-server';
+import auth from 'json-server-auth'
 const { create, router: _router, defaults } = pkg;
 
 // const cert = fs.readFileSync('.cert/certificate.crt');
@@ -15,7 +16,20 @@ const server = create();
 const router = _router('list_of_events.json');
 const middlewares = defaults();
 
+server.db = router.db
+
+const rules = auth.rewriter({
+  // Permission rules
+  users: 600,
+  Events: 440,
+  Associations: 440,
+  Sources: 440
+  // Other rules
+})
+
 server.use(middlewares);
+server.use(rules)
+server.use(auth)
 server.use(router);
 
 const PORT = 3000; // Change the port if needed
