f558a6c7e54715203eeec1aff7086eb58aa28e9f
Vasile <popovvasile@gmail.com>
2023-12-11 01:51:50 +0200

    changing api to https

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://api.osintforukraine.com/",
+        target: "https://api.osintforukraine.com/",
         changeOrigin: true,
       },
     }
