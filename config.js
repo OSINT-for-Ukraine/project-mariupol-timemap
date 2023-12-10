e2bacad3e7c52a988a7ee14c1bfc7e390b92b377
Vasile <popovvasile@gmail.com>
2023-12-11 01:08:37 +0200

    changing api to https

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const one_day = 1440;
 const config = {
   title: "Project Mariupol",
   display_title: "Project Mariupol",
-  SERVER_ROOT: "http://api.osintforukraine.com/",
+  SERVER_ROOT: "https://api.osintforukraine.com/",
   EVENTS_EXT: "Events",
   SOURCES_EXT: "Sources",
   ASSOCIATIONS_EXT: "Associations",
   LOGIN_EXT: "login",
-  API_DATA: "http://api.osintforukraine.com/Events",
+  API_DATA: "https://api.osintforukraine.com/Events",
   MAPBOX_TOKEN:
     "pk.eyJ1IjoiYmVsbGluZ2NhdC1tYXBib3giLCJhIjoiY2tleW0wbWliMDA1cTJ5bzdkbTRraHgwZSJ9.GJQkjPzj8554VhR5SPsfJg",
   // MEDIA_EXT: "/api/media",
