8ece65b6a39d7e275f1e3fbfb21ea465be0e92b9
Catalina <catalinahasnas1996@gmail.com>
2024-03-03 11:05:42 +0200

    remove all clusters when at least  one event is selected

--- src/components/space/carto/atoms/Events.jsx
+++ src/components/space/carto/atoms/Events.jsx
@@ -116,19 +116,10 @@ function MapEvents({
     );
   }
 
-  const selectedEventInLocation = locations.filter((location) =>
-    selected.some((event) => location.id === event.id)
-  );
-
-  const updatedLocations =
-    selectedEventInLocation.length > 0 ? selectedEventInLocation : locations;
-
   return (
     <Portal node={svg}>
       <svg>
-        <g className="event-locations">
-          {updatedLocations.map(renderLocation)}
-        </g>
+        <g className="event-locations">{locations.map(renderLocation)}</g>
       </svg>
     </Portal>
   );
