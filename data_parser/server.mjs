94c62efc26132406c2f9a0630582cba42f7917d4
Vasile <popovvasile@gmail.com>
2024-03-03 22:33:18 +0100

    api.json

--- data_parser/server.mjs
+++ data_parser/server.mjs
@@ -4,7 +4,7 @@ const { create, router: _router, defaults } = pkg;
 
 
 const server = create();
-const router = _router('list_of_events.json');
+const router = _router('api.json');
 const middlewares = defaults();
 
 server.db = router.db
