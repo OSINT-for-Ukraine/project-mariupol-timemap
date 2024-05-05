5c288df2a079b9ac916a072c5d3f8bda6531ec54
Vasile <popovvasile@gmail.com>
2024-05-05 16:59:30 +0200

    test prod

--- vite.config.js
+++ vite.config.js
@@ -1,66 +1,36 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import compressionPlugin from 'vite-plugin-compression';
 
-//https://vitejs.dev/config/#using-environment-variables-in-config
-const env = loadEnv('','');
-const api_url = JSON.stringify(env.VITE_API_URL);
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const api_url = env.VITE_API_URL;
 
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "build"
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: api_url,
-        changeOrigin: true,
-      },
-    }
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./test/setup.js",
-    passWithNoTests: true
-  },
+  return {
+    plugins: [
+      react(),
+      compressionPlugin({
+        disable: true,  // Disable or configure appropriately
+      })
+    ],
+    build: {
+      outDir: 'build',
+      sourcemap: true,
+    },
+    server: {
+      proxy: {
+        '/api/': {
+          target: api_url,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      }
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './test/setup.js',
+      passWithNoTests: true
+    },
+  };
 });
-
-// import { defineConfig, loadEnv } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import compressionPlugin from 'vite-plugin-compression';
-//
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd());
-//   const api_url = env.VITE_API_URL;
-//
-//   return {
-//     plugins: [
-//       react(),
-//       compressionPlugin({
-//         disable: true,  // Disable or configure appropriately
-//       })
-//     ],
-//     build: {
-//       outDir: 'build',
-//       sourcemap: true,
-//     },
-//     server: {
-//       proxy: {
-//         '/api/': {
-//           target: api_url,
-//           changeOrigin: true,
-//           rewrite: path => path.replace(/^\/api/, ''),
-//         },
-//       }
-//     },
-//     test: {
-//       globals: true,
-//       environment: 'jsdom',
-//       setupFiles: './test/setup.js',
-//       passWithNoTests: true
-//     },
-//   };
-// });
